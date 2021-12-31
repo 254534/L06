30d2bf8dfdb9683e0637f4562fdc38e3b9f27e3a
ksproska <kamila.sproska@gmail.com>
2021-12-31 14:36:21 +0100

    fab added, list displays people

--- app/src/main/java/com/example/l06/Fragment3.kt
+++ app/src/main/java/com/example/l06/Fragment3.kt
@@ -8,15 +8,30 @@ import android.view.ViewGroup
 import android.widget.*
 
 class Fragment3 : Fragment() {
-    private var ltxt1 = arrayOf("Dzień dobry", "Bonjour", "Hola", "Zdravstvuyte", "Nǐn hǎo", "Salve", "Konnichiwa")
-    private var ltxt2 = arrayOf("polski", "francuski", "hiszpański", "rosyjski", "chiński", "włoski", "japoński")
-    private var limg = arrayOf(R.drawable.z1, R.drawable.z2, R.drawable.z3, R.drawable.z4, R.drawable.z1, R.drawable.z4, R.drawable.z3)
+    private var names = arrayOf("Yahya O'Brien", "Anne-Marie Sharpe", "Vanessa Raymond", "Kerri Estrada", "Trixie Greenwood", "Ariyan Gaines", "Kinga Stark")
+    private var occupation = arrayOf("architekt", "informatyk", "informatyk", "nauczyciel", "taksówkarz", "fryzjer", "architekt")
+    private var gender = arrayListOf<Boolean>(false, true, true, true, true, false, true)
+    private var age = arrayListOf<Int>(15, 23, 11, 23, 33, 45, 21)
+
+    fun <T> append(arr: Array<T>, element: T): Array<T?> {
+        val array = arr.copyOf(arr.size + 1)
+        array[arr.size] = element
+        return array
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var listView = view.findViewById<ListView>(R.id.main_listview)
         if (listView != null) run {
-            var adapter = CustomListViewAdapter(view.context, ltxt1, ltxt2, limg)
+            var limg: Array<Int?> = arrayOf()
+            for(g in gender) {
+                limg = if(g) {
+                    append(limg, R.drawable.z1)
+                } else {
+                    append(limg, R.drawable.z2)
+                }
+            }
+            var adapter = CustomListViewAdapter(view.context, names, occupation, limg as Array<Int>)
             listView.adapter = adapter
             listView.onItemClickListener = object : AdapterView.OnItemLongClickListener,
                 AdapterView.OnItemClickListener {
@@ -37,7 +52,7 @@ class Fragment3 : Fragment() {
                     id: Long
                 ) {
                     if (view != null) {
-                        Toast.makeText(view.context, ltxt1[position], Toast.LENGTH_LONG).show()
+                        Toast.makeText(view.context, names[position], Toast.LENGTH_LONG).show()
                     }
                 }
             }
