feb7463bb4af9f4b59ab45755cdaac71a50a8488
ksproska <kamila.sproska@gmail.com>
2022-01-01 10:02:19 +0100

    all elements to edit present

--- app/src/main/java/com/example/l06/EditActivity.kt
+++ app/src/main/java/com/example/l06/EditActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.widget.EditText
+import android.widget.*
 
 class EditActivity : AppCompatActivity() {
     private var position: Int? = null
@@ -31,6 +31,20 @@ class EditActivity : AppCompatActivity() {
         rating = bundle.getFloat("rating")
 
         val editName: EditText = findViewById(R.id.editTextName)
+        val editOccupation: EditText = findViewById(R.id.editOccupation)
+        val radioButtonWoman: RadioButton = findViewById(R.id.radioButtonWoman)
+        val radioButtonMan: RadioButton = findViewById(R.id.radioButtonMan)
+        val imageView: ImageView = findViewById(R.id.imageViewEdit)
+        val ratingBar: RatingBar = findViewById(R.id.ratingBarE)
+        val textColor: TextView = findViewById(R.id.textColor2)
+
         editName.text = name?.toEditable()
+
+        if (gender == true) radioButtonWoman.isChecked = true else radioButtonMan.isChecked = true
+        imageView.setImageResource(if(gender == true) R.drawable.woman else R.drawable.man)
+        editOccupation.text = occupation?.toEditable()
+        textColor.setBackgroundColor(color!!)
+        ratingBar.numStars = 5
+        ratingBar.rating = rating!!
     }
 }
